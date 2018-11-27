2f9f9fd0122dbbebcb8a97fafa020900f596c6a7
Bryson Spaulding <brysonian10@hotmail.com>
2018-11-27 08:22:09 -0700

    Added a lot of different things to FirstPanel.

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -1,6 +1,34 @@
 package gui.view;
 
-public class FirstPanel
-{
+import gui.controller.GUIController;
+import javax.swing.*;
 
+public class FirstPanel extends JPanel
+{
+	private GUIController appController;
+	
+	public FirstPanel(GUIController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel() 
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
