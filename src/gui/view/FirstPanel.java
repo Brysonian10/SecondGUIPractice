ac8b57a4a109cfff388ef08425380e42c37082c2
Bryson Spaulding <brysonian10@hotmail.com>
2018-11-27 09:05:24 -0700

    Made it so the background color changes everytime the button is pressed.

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -48,4 +48,14 @@ public class FirstPanel extends JPanel
 					}
 				});
 	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red is: " + red + " Blue is: " + blue + " Green is: " + green);
+	}
 }
