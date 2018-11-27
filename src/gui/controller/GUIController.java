90657ab0477fb92bf2dc1a84286413207734fba3
Bryson Spaulding <brysonian10@hotmail.com>
2018-11-27 08:29:12 -0700

    Added stuff to the controller that makes it work with the FirstFrame and the FirstPanel.

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,7 +1,18 @@
 package gui.controller;
 
+import gui.view.FirstFrame;
+import gui.view.FirstPanel;
+
 public class GUIController
 {
+	private FirstFrame appFrame;
+	private FirstPanel appPanel;
+	
+	public GUIController()
+	{
+		this.appFrame = new FirstFrame(this);
+	}
+	
 	public void start()
 	{
 		
