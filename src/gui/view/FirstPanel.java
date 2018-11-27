92980373cbae6908e874d63130a01f97926af781
Bryson Spaulding <brysonian10@hotmail.com>
2018-11-27 08:48:03 -0700

    Added some more changes to FirstPanel that now has a button and a label.

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,15 +2,22 @@ package gui.view;
 
 import gui.controller.GUIController;
 import javax.swing.*;
+import java.awt.Color;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController appController;
 	
+	private JButton colorButton;
+	private JLabel textLabel;
+	
 	public FirstPanel(GUIController appController)
 	{
 		super();
 		this.appController = appController;
+		
+		colorButton = new JButton("Click to change the color");
+		textLabel = new JLabel("This is a color app");
 		
 		setupPanel();
 		setupLayout();
@@ -19,7 +26,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel() 
 	{
-		
+		this.add(textLabel);
+		this.add(colorButton);
+		this.setBackground(Color.BLUE);
 	}
 	
 	private void setupLayout()
